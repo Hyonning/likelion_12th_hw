0e3d130d9b9f19b259e4f24fb686f78d179a04fb
2021110765@dgu.ac.kr <2021110765@dgu.ac.kr>
2024-04-08 23:43:05 +0900

    FE_CHE_2nd hw

--- project/main/views.py
+++ project/main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mainpage(request):
+    context = {
+        'generation': 12,
+        'MTV': ['model', 'templates', 'view'],
+        'info':{'model': '데이터', 'templates': '보여지는 부분', 'view': '요청에 따른 적절한 로직 수행'},
+        'members':{'request','url conf', 'view','model','template','response'}
+    }
+    return render(request, 'main/mainpage.html', context)
+
+def secondpage(request) :
+    return render(request, 'main/secondpage.html')
+
